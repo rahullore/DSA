8536f185e02c27529c22278e0138a032489d5450
rahullore <rahullore@gmail.com>
2022-07-02 22:50:07 -0400

    code

---
+++ Smart.work.Test/Meetingroom/MeetingroomTest.cs
@@ -0,0 +1,69 @@
+using Smart.work.Meetingroom;
+
+namespace Smart.work.Test.Meetingroom
+{
+    public class MeetingroomTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void CheckWhenThereIsNoAcivity()
+        {
+            LogTimeHandler logTimeHandler = new LogTimeHandler(new List<Logtime>());
+
+            Assert.That(logTimeHandler.GetIdleTime(), Is.EqualTo(24 * 60));
+        }
+
+        [Test]
+        public void CheckWhenThereIsOnlyOneAcivity()
+        {
+
+            LogTimeHandler logTimeHandler = new LogTimeHandler(new List<Logtime>()
+            { 
+                new Logtime {
+                    Start= new DateTimeOffset(2022,07,02,10,00,00,TimeSpan.Zero),
+                    End = new DateTimeOffset(2022,07,02,12,00,00,TimeSpan.Zero),
+                },                
+            });
+
+            Assert.That(logTimeHandler.GetIdleTime(), Is.EqualTo(22 * 60));//because 2 hours it's busy
+        }
+
+        [Test]
+        public void CheckWhenThereIsMultipleAcivity()
+        {
+
+            LogTimeHandler logTimeHandler = new LogTimeHandler(new List<Logtime>()
+            {
+                new Logtime {
+                    Start= new DateTimeOffset(2022,07,02,02,00,00,TimeSpan.Zero),
+                    End = new DateTimeOffset(2022,07,02,03,00,00,TimeSpan.Zero),
+                },
+
+                 new Logtime {
+                    Start= new DateTimeOffset(2022,07,02,02,30,00,TimeSpan.Zero),
+                    End = new DateTimeOffset(2022,07,02,03,30,00,TimeSpan.Zero),
+                },
+
+                  new Logtime {
+                    Start= new DateTimeOffset(2022,07,02,04,00,00,TimeSpan.Zero),
+                    End = new DateTimeOffset(2022,07,02,05,00,00,TimeSpan.Zero),
+                },
+
+                    new Logtime {
+                    Start= new DateTimeOffset(2022,07,02,02,00,00,TimeSpan.Zero),
+                    End = new DateTimeOffset(2022,07,02,05,00,00,TimeSpan.Zero),
+                },
+            });
+
+            double busyTime = 3 * 60;
+
+            Assert.That(logTimeHandler.GetIdleTime(), Is.EqualTo((24 * 60) - busyTime));
+        }
+
+    }
+}
---
+++ Smart.work/Meetingroom/Logtime.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart.work.Meetingroom
+{
+    public class Logtime: IComparable<Logtime>
+    {
+        public DateTimeOffset Start { get; set; }
+        public DateTimeOffset End { get; set; }
+        public Logtime()
+        {
+
+        }
+
+        public int CompareTo(Logtime? other)
+        {
+            if(other == null) return 1;
+            else
+            {
+                return this.Start.CompareTo(other.Start);
+            }
+        }
+    }
+
+    public class LogTimeHandler
+    {
+        public List<Logtime> Logs { get; private set; }
+        public LogTimeHandler(List<Logtime> logs)
+        {
+            Logs = logs;
+        }
+
+        public double GetIdleTime()
+        {
+            double idleTime = 24*60;
+            this.Logs.Sort();
+            if (Logs.Count == 0) return idleTime;
+            if(Logs.Count==1) return idleTime - (Logs[0].End - Logs[0].Start).TotalMinutes;
+
+            Logtime prev = null;
+            
+            foreach (var curr in Logs)
+            {
+                if(prev==null)//first element 
+                {
+                    idleTime -= (curr.End - curr.Start).TotalMinutes;
+                    prev = curr;
+                }
+                else
+                {
+                    if(curr.Start<prev.End && curr.End <= prev.End)
+                    {
+                        continue;
+                    }
+                    else if(curr.Start < prev.End)
+                    {
+                        idleTime -= (curr.End - prev.End).TotalMinutes;                        
+                    }
+                    else if(curr.Start >= prev.End)
+                    {
+                        idleTime -= (curr.End - prev.End).TotalMinutes;
+                    }
+                    prev = new Logtime { Start = prev.Start, End = curr.End };
+                }
+            }
+           
+
+            return idleTime;
+
+        }
+    }
+}
